4a68e240dde33f8d0afa5d27f1b9e5a64f9d60e7
Juan Diego Derewicki <88844221+JuandieDerewicki@users.noreply.github.com>
2023-07-18 12:49:00 -0300

    Add files via upload

---
+++ Asincronismo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        Console.WriteLine("Inicio del programa");
+        await EjecutarTareaAsincrona();
+        Console.WriteLine("Fin del programa");
+    }
+
+    public static async Task EjecutarTareaAsincrona()
+    {
+        Console.WriteLine("Iniciando tarea asincrónica");
+        await Task.Delay(2000); // Simulación de una operación asincrónica que tarda 2 segundos
+        Console.WriteLine("Tarea asincrónica completada");
+    }
+}
+//public class Program
+//{
+//    public static async Task Main()
+//    {
+//        Console.WriteLine("Inicio del programa");
+
+//        string resultado = await ObtenerDatosDeApi();
+//        Console.WriteLine(resultado);
+
+//        Console.WriteLine("Fin del programa");
+//    }
+
+//    public static async Task<string> ObtenerDatosDeApi()
+//    {
+//        using (HttpClient cliente = new HttpClient())
+//        {
+//            Console.WriteLine("Iniciando solicitud a la API");
+//            HttpResponseMessage respuesta = await cliente.GetAsync("https://api.example.com/data");
+//            respuesta.EnsureSuccessStatusCode();
+//            string contenido = await respuesta.Content.ReadAsStringAsync();
+//            Console.WriteLine("Solicitud a la API completada");
+//            return contenido;
+//        }
+//    }
+//}
