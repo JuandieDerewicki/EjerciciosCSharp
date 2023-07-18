c5b5c1abc4e90b180f0f3fa469b69f408313a662
Juan Diego Derewicki <88844221+JuandieDerewicki@users.noreply.github.com>
2023-07-18 12:49:48 -0300

    Add files via upload

---
+++ DataSets Tipados/DataSet1.cs
@@ -0,0 +1,7 @@
+﻿namespace DataSetsTipados
+{
+}
+
+namespace DataSetsTipados
+{
+}
---
+++ DataSets Tipados/Form1.cs
@@ -0,0 +1,10 @@
+namespace DataSetsTipados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
