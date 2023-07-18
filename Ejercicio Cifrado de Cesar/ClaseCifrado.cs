877b115638c00ca636bca92e1664a8a0f2ccff19
Juan Diego Derewicki <88844221+JuandieDerewicki@users.noreply.github.com>
2023-07-18 12:35:19 -0300

    Add files via upload

---
+++ Ejercicio Cifrado de Cesar/ClaseCifrado.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioCifradoCesar2
+{
+    internal class ClaseCifrado
+    {
+
+        //private string palabra; Son innecesarios pq ya se asignan valores e inicializan desde el constructor. Ademas estos campos privados ni siquiera son utilizados durante la clase, por lo que es innecesario declararlos de esta manera
+        //private int clave;
+
+        public ClaseCifrado(string palabra, int clave)
+        {
+            this.Palabra = palabra;
+            this.Clave = clave; 
+        }
+
+        string Palabra { get; set; }
+        int Clave { get; set; }
+        public string recorrer(int password, char texto, bool opcion) // Va a recibir la clave de cuantos lugares moverse, la palabra y un booleano que nos va a decir si es + o -.
+        {
+            string caracteresCifrados = "aábcdeéfghiíjklmnñoópqrstuúüvwxyz";
+            int cont = 0;
+            int indice = caracteresCifrados.IndexOf(texto); // Lo que buscamos es el primer indice del string texto que se encuentra en "caracteres cifrados". Ej; si la palabra es barco el primer indice es 2
+            int ultimo = caracteresCifrados.Length - 1; // Para saber el numero de letras de caracteres cifrados y lo hacemos - 1 porque arranca en 0.
+            
+            while(cont < password) // Mientras cont sea menor a la clave, seguimos iterando y devolviendo letra por letra.
+            {
+                if (opcion) // Si opcion es true, significa que el numero es positivo y se debe mover por el numero de clave
+                {
+                    indice++;
+                }
+                else // Si opcion es false, significa que el numero es negativo y se debe mover por el numero de clave
+                {
+                    indice--;
+                }
+                if (indice > ultimo) // Si nos pasamos de la cantidad de caracteres que tenemos en caracteres cifrados que es 28, volvemos a empezar en 0
+                {
+                    indice = 0;
+                }
+                if (indice < 0) // Si nos pasamos de la cantidad de caracteres que tenemos en caracteres cifrados, vamos al ultimo. Esto pasa cuando nos movemos hacia la izquierda.
+                {
+                    indice = ultimo;
+                }
+                cont++; // Aumenta cont y volvemos a repetir el proceso.
+            }
+            return caracteresCifrados[indice].ToString(); // El método devuelve una letra en cada llamada, por lo que si quieres cifrar una palabra, debes llamar a este método por cada letra de la palabra y concatenar las letras resultantes para formar la palabra cifrada
+            /* Forma 2
+             
+            */ 
+        }
+
+        public string Encriptar()
+        {
+            string textoEncriptado = "";
+            foreach (char letra in Palabra)
+            {
+                if(letra == ' ')
+                {
+                    textoEncriptado += " ";
+                }
+                else
+                {
+                    string letraEncriptada = recorrer(this.Clave, letra, true);
+                    textoEncriptado += letraEncriptada.ToString();
+                }
+            }
+            return textoEncriptado;
+        }
+        /* Forma 2
+        String aux = "";
+        for (int i = 0; i < this.entrada.Length; i++)
+        {
+            if (this.entrada[i] == ' ')
+            {
+                aux = aux + " ";
+            }
+            else
+            {
+                aux = aux + this.recorrer(this.password, this.entrada[i], true);
+            }
+        }
+        return aux;
+        EXPLICACION:
+        Este método recibe un texto (almacenado en la variable entrada) y lo recorre caracter por caracter, aplicando la función recorrer para encriptar cada letra.
+        Si la letra es un espacio, se agrega directamente a la variable aux que se utiliza para almacenar el texto encriptado. Si no es un espacio, se llama a la función recorrer pasando la clave y la letra actual, con un tercer parámetro de true que indica que se debe mover hacia la derecha en el alfabeto. El resultado de recorrer se agrega a aux.
+        Finalmente, el método devuelve la variable aux que contiene el texto encriptado.
+        */
+        public string Desencriptar()
+        {
+            string textoEncriptado = "";
+            foreach (char letra in Palabra)
+                if (letra == ' ')
+                {
+                    textoEncriptado += " ";
+                }
+                else
+                {
+                    string letraEncriptada = recorrer(this.Clave, letra, false);
+                    textoEncriptado += letraEncriptada.ToString();
+                }
+            return textoEncriptado;
+        }
+        /* Forma 2
+        String aux = "";
+        for (int i = 0; i < this.entrada.Length; i++)
+        {
+            if (this.entrada[i] == ' ')
+            {
+                aux = aux + " ";
+            }
+            else
+            {
+                aux = aux + this.recorrer(this.password, this.entrada[i], false);
+            }
+        }
+        return aux;
+        */
+    }
+}
---
+++ Ejercicio Cifrado de Cesar/Program.cs
@@ -0,0 +1,191 @@
+﻿
+using EjercicioCifradoCesar2;
+
+string palabra;
+int contraseña = 0;
+int opcion = 0; 
+bool cond = true;
+bool cond2 = true;
+
+
+do
+{
+    Console.WriteLine("Ingrese una palabra: ");
+    palabra = Console.ReadLine();
+
+    if (palabra.Replace(" ", "").All(c => Char.IsLetter(c)))
+    {
+        break;
+    }
+    Console.WriteLine("Ingrese una palabra válida\n");
+} while (true);
+
+
+while (cond)
+{
+    Console.WriteLine("Ingrese una contraseña válida en números:");
+    if (int.TryParse(Console.ReadLine(), out contraseña))
+    {
+        cond = false;
+    }
+    else
+    {
+        Console.WriteLine("Error al ingresar contraseña - Ingrese una contraseña válida en números\n");
+    }
+}
+
+ClaseCifrado c1 = new ClaseCifrado(palabra, contraseña);
+
+
+while (cond2)
+{
+    Console.WriteLine("Ingrese 1 si desea encriptar\nIngrese 2 si desea desencriptar");
+    if (int.TryParse(Console.ReadLine(), out opcion))
+    {
+        cond2 = false;
+    }
+    else
+    {
+        Console.WriteLine("Error al ingresar contraseña - Ingrese una contraseña válida en números\n");
+    }
+}
+
+if (opcion == 1)
+{
+    Console.WriteLine(c1.Encriptar());
+}
+else if (opcion == 2)
+{
+    Console.WriteLine(c1.Desencriptar());
+}
+else
+{
+    Console.WriteLine("El numero no se encuentra dentro de los parametros");
+}
+
+/*
+using System;
+
+namespace CifradoCesar
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ingrese una palabra: ");
+            String palabra = Console.ReadLine();
+            Console.WriteLine("Ingrese la contraseña: ");
+            int password;
+            if (!(int.TryParse(Console.ReadLine(), out password)))
+            {
+                Console.WriteLine("Error al ingresar password");
+            }
+            else
+            {
+                Cesar cesar = new Cesar(palabra, password);
+                Console.WriteLine("Para encriptar ingrese 1. Para desencriptar ingrese 2.");
+                int opcion;
+                if ((int.TryParse(Console.ReadLine(), out opcion)))
+                {
+                    if (opcion == 1)
+                    {
+                        Console.WriteLine(cesar.encriptar());
+                    }
+                    else if (opcion == 2)
+                    {
+                        Console.WriteLine(cesar.desencriptar());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Opcion no valida");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ingrese un numero de las opciones");
+                }
+            }
+        }
+    }
+
+    public class Cesar
+    {
+        String entrada { get; set; }
+        int password { get; set; }
+
+        public Cesar(String entrada, int n)
+        {
+            this.entrada = entrada;
+            this.password = n;
+        }
+
+        private String recorrer(int repetir, Char indice, bool opcion)
+        {
+            String caracteres = "aábcdeéfghiíjklmnñoópqrstuúüvwxyz";
+            int cont = 0;
+            int i = caracteres.IndexOf(indice);
+            int ultimo = caracteres.Length - 1;
+            while (cont < repetir)
+            {
+                if (opcion)
+                {
+                    i++;
+                }
+                else
+                {
+                    i--;
+                }
+                if (i < 0)
+                {
+                    i = ultimo;
+                }
+                if (i > ultimo)
+                {
+                    i = 0;
+                }
+                cont++;
+            }
+            return caracteres[i].ToString();
+        }
+
+        public String encriptar()
+        {
+            String aux = "";
+            for (int i = 0; i < this.entrada.Length; i++)
+            {
+                if (this.entrada[i] == ' ')
+                {
+                    aux = aux + " ";
+                }
+                else
+                {
+                    aux = aux + this.recorrer(this.password, this.entrada[i], true);
+                }
+            }
+            return aux;
+        }
+
+        public String desencriptar()
+        {
+            String aux = "";
+            for (int i = 0; i < this.entrada.Length; i++)
+            {
+                if (this.entrada[i] == ' ')
+                {
+                    aux = aux + " ";
+                }
+                else
+                {
+                    aux = aux + this.recorrer(this.password, this.entrada[i], false);
+                }
+            }
+            return aux;
+        }
+    }
+}
+*/
+
+
+
+
+
