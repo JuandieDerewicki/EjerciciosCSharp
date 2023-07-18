e86eeeea57accb905cc815cf4a0a44ef52e9cf15
Juan Diego Derewicki <88844221+JuandieDerewicki@users.noreply.github.com>
2023-07-18 12:50:52 -0300

    Add files via upload

---
+++ Delegados/Program.cs
@@ -0,0 +1,57 @@
+﻿// Declaracion del tipo 
+public delegate void MiDelegado(string msj);
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        //Instanciacion - "method group conversion syntax"
+//        MiDelegado delegado = ClaseA.MetodoA;
+//        //Invocacion
+//        delegado("Soy el delegado");
+//    }
+//}
+
+//class ClaseA
+//{
+//    public static void MetodoA(string mensaje)
+//    {
+//        Console.WriteLine("Llamado ClaseA.MetodoA() con parámetro: " + mensaje);
+//    }
+//}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Instanciacion - "method group conversion syntax"
+        MiDelegado delegado = ClaseA.MetodoA;
+        //Invocacion
+        //delegado("Invocado con este parámetro");
+
+        //delegado = ClaseB.MetodoB;  // Sobreescribiría al metodo A
+        delegado += ClaseB.MetodoB; // Se concatenaria con el metodo A e imprimiría los dos
+        InvocarDelegado(delegado);
+    }
+    static void InvocarDelegado(MiDelegado del)
+    {
+        del("Soy el delegado"); // Delegado
+    }
+}
+
+class ClaseA
+{
+    public static void MetodoA(string mensaje)
+    {
+        Console.WriteLine("Llamado ClaseA.MetodoA() con parámetro: " + mensaje);
+    }
+}
+
+class ClaseB
+{
+    public static void MetodoB(string mensaje)
+    {
+        Console.WriteLine("Llamado ClaseA.MetodoB() con parámetro: " + mensaje);
+    }
+}
+
