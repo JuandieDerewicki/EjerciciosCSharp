b5794479b7df33f4638358ebf3062af18b697a4a
Juan Diego Derewicki <88844221+JuandieDerewicki@users.noreply.github.com>
2023-07-18 12:47:22 -0300

    Add files via upload

---
+++ Ejercicio Manejo de Tiempo y Fechas/Class1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManejoTiempoYFechas
+{
+    internal class Class1
+    {
+
+        DateTime[] feriados = new DateTime[]
+        {
+            new DateTime(2023, 1, 1),  // Año Nuevo
+            new DateTime(2023, 2, 20), // Carnaval
+            new DateTime(2023, 2, 21), // Carnaval
+            new DateTime(2023, 3, 24), // Dia de la memoria 
+            new DateTime(2023, 4, 6), // Jueves Santo
+            new DateTime(2023, 4, 7), // Viernes Santo
+            new DateTime(2023, 5, 1),  // Día del Trabajo
+            new DateTime(2023, 5, 25),  // 25 de mayo
+            new DateTime(2023, 5, 26)  // Feriado puente
+        };
+        public void ObtenerDiasCalendario(DateTime fecha1, DateTime fecha2)
+        {
+            // Validar que fecha2 sea mayor que fecha1
+            if (fecha2 < fecha1)
+            {
+                // Intercambiar las fechas
+                DateTime temp = fecha1;
+                fecha1 = fecha2;
+                fecha2 = temp;
+            }
+            TimeSpan dias = fecha2 - fecha1;
+            Console.WriteLine("Los dias entre esas dos fechas son " + dias.Days);
+            //return dias.Days;
+        }
+
+        public void ObtenerDiasLaborales(DateTime fecha1, DateTime fecha2)
+        {
+            // Validar que fecha2 sea mayor que fecha1
+            if (fecha2 < fecha1)
+            {
+                // Intercambiar las fechas
+                DateTime temp = fecha1;
+                fecha1 = fecha2;
+                fecha2 = temp;
+            }
+
+            /*
+            int diasLaborales = 0;
+            DateTime fechaActual = fecha1;
+
+            while (fechaActual <= fecha2)
+            {
+            if (fechaActual.DayOfWeek != DayOfWeek.Saturday && fechaActual.DayOfWeek != DayOfWeek.Sunday && !feriados.Contains(fechaActual))
+            {
+                diasLaborales++;
+            }
+
+            fechaActual = fechaActual.AddDays(1);
+            }
+            */
+
+            int diasLaborales = 0;
+            for (DateTime fecha = fecha1; fecha <= fecha2; fecha = fecha.AddDays(1))
+            {
+                if (fecha.DayOfWeek != DayOfWeek.Saturday && fecha.DayOfWeek != DayOfWeek.Sunday && !feriados.Contains(fecha))
+                {
+                    diasLaborales++;
+                }
+            }
+
+            Console.WriteLine($"Los días laborales entre las fechas {fecha1.ToShortDateString()} y {fecha2.ToShortDateString()} son: {diasLaborales}");
+        }
+
+        public void SumarDiasLaborales(DateTime fecha1, int diasSumados)
+        {
+            Console.WriteLine("La nueva fecha es " + fecha1.AddDays(diasSumados).ToShortDateString());
+        }
+
+
+    }
+}
---
+++ Ejercicio Manejo de Tiempo y Fechas/Program.cs
@@ -0,0 +1,131 @@
+﻿using ManejoTiempoYFechas;
+
+Class1 c1 = new Class1();
+
+bool fechaValida = false; 
+bool fechaValida2 = false;
+bool fechaValida3 = false;
+int opcion;
+int diasSumados;
+bool opcionCond = false;
+DateTime fecha1 = new DateTime();
+DateTime fecha2 = new DateTime();
+
+
+
+Console.WriteLine("Indique que desea hacer:\n1-Obtener dias entre dos fechas\n2-Obtener dias laborales entre dos fechas\n3-Sumar dias laborales");
+opcion = int.Parse(Console.ReadLine());
+while (opcionCond == false)
+{
+    if (opcion == 1)
+    {
+        Console.WriteLine("---Ingrese dos fechas para obtener los dias entre ellos---");
+
+        while (fechaValida == false)
+        {
+            Console.WriteLine("Ingresa la primer fecha en el formato dd/MM/yyyy:");
+            if (DateTime.TryParse(Console.ReadLine(), out fecha1))
+            {
+                fechaValida = true;
+            }
+            else
+            {
+                Console.WriteLine("La fecha ingresada no es válida. Por favor intente nuevamente.\n");
+            }
+        }
+
+        while (fechaValida2 == false)
+        {
+            Console.WriteLine("Ingresa la segunda fecha en el formato dd/MM/yyyy:");
+            if (DateTime.TryParse(Console.ReadLine(), out fecha2))
+            {
+                fechaValida2 = true;
+            }
+            else
+            {
+                Console.WriteLine("La fecha ingresada no es válida. Por favor intente nuevamente.\n");
+            }
+        }
+        c1.ObtenerDiasCalendario(fecha1, fecha2);
+        opcionCond = true;
+    }
+    else if (opcion == 2)
+    {
+        Console.WriteLine("---Ingrese dos fechas para obtener los dias entre ellos---");
+
+        while (fechaValida == false)
+        {
+            Console.WriteLine("Ingresa la primer fecha en el formato dd/MM/yyyy:");
+            if (DateTime.TryParse(Console.ReadLine(), out fecha1))
+            {
+                fechaValida = true;
+            }
+            else
+            {
+                Console.WriteLine("La fecha ingresada no es válida. Por favor intente nuevamente.\n");
+            }
+        }
+
+        while (fechaValida2 == false)
+        {
+            Console.WriteLine("Ingresa la segunda fecha en el formato dd/MM/yyyy:");
+            if (DateTime.TryParse(Console.ReadLine(), out fecha2))
+            {
+                fechaValida2 = true;
+            }
+            else
+            {
+                Console.WriteLine("La fecha ingresada no es válida. Por favor intente nuevamente.\n");
+            }
+        }
+        c1.ObtenerDiasLaborales(fecha1, fecha2);
+        opcionCond = true;
+    }
+    else if (opcion == 3)
+    {
+        while (fechaValida3 == false)
+        {
+            Console.WriteLine("Ingresa la primer fecha en el formato dd/MM/yyyy:");
+            if (DateTime.TryParse(Console.ReadLine(), out fecha1))
+            {
+                fechaValida3 = true;
+            }
+            else
+            {
+                Console.WriteLine("La fecha ingresada no es válida. Por favor intente nuevamente.\n");
+            }
+        }
+        Console.WriteLine("Indique cuantos dias le desea sumar");
+        diasSumados = int.Parse(Console.ReadLine());
+
+        c1.SumarDiasLaborales(fecha1, diasSumados);
+        opcionCond = true;    
+    }
+    else
+    {
+        Console.WriteLine("ERROR. Ingrese una opcion valida:");
+        opcion = int.Parse(Console.ReadLine());
+    }
+}
+
+
+
+/*
+DateTime fecha1 = new DateTime(2023, 04, 23);
+DateTime fecha2 = new DateTime(2023, 04, 20);
+
+c1.ObtenerDiasCalendario(fecha1, fecha2);
+*/
+
+/*
+ Console.WriteLine("Por favor ingrese una fecha en el formato dd/MM/yyyy:");
+DateTime fecha;
+if (DateTime.TryParse(Console.ReadLine(), out fecha))
+{
+    Console.WriteLine("La fecha ingresada es: " + fecha.ToShortDateString());
+}
+else
+{
+    Console.WriteLine("La fecha ingresada no es válida.");
+}
+ */
