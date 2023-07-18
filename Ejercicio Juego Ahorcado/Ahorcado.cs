68968897d52008ee389e1393797ad64480deacdf
Juan Diego Derewicki <88844221+JuandieDerewicki@users.noreply.github.com>
2023-07-18 12:43:29 -0300

    Add files via upload

---
+++ Ejercicio Juego Ahorcado/Ahorcado.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuegoAhorcado
+{
+    internal class Ahorcado
+    {
+        int contLetras = 0;
+        bool ganador = false;
+        private string[] palabrasJuego = new string[10] { "pato", "mesa", "vela", "hoja", "pelo", "gato", "vaca", "vaso", "edad", "veda" };
+        string palabra;
+        string palabraGuiones = "";
+        public Ahorcado()
+        {
+            palabraAhorcado();
+            generarGuiones();
+        }
+        public string Palabra { get => palabra; set => palabra = value; }
+        
+        public void palabraAhorcado()
+        {
+            Random palabraRandom = new Random();
+            int indiceAleatorio = palabraRandom.Next(palabrasJuego.Length);
+            Palabra = palabrasJuego[indiceAleatorio];
+        }
+
+        public void generarGuiones()
+        {
+            for (int i = 0; i < Palabra.Length; i++)
+            {
+                if (i == 0)
+                {
+                    palabraGuiones = "_ ";
+                }
+                else
+                {
+                    palabraGuiones += "_ ";
+                }
+            }
+            Console.WriteLine($"La palabra tiene {Palabra.Length} letras y es {Palabra}");
+            Console.WriteLine(palabraGuiones);
+        }
+        public void agregarLetra(Char letraAgregada)
+        {
+            string nuevaPalabra = "";
+            for (int i = 0; i < Palabra.Length; i++)
+            {
+                if (Palabra[i] == letraAgregada)
+                {
+                    nuevaPalabra += letraAgregada + " ";
+                    contLetras++;
+                }
+                else
+                {
+                    nuevaPalabra += palabraGuiones[i * 2] + " ";
+                }
+            }
+            palabraGuiones = nuevaPalabra;
+            Console.WriteLine(palabraGuiones);
+        }
+        public bool comprobarGanador()
+        {
+            if (contLetras == Palabra.Length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Ejercicio Juego Ahorcado/Program.cs
@@ -0,0 +1,22 @@
+﻿using JuegoAhorcado;
+
+int contIntentos = 0;   
+Console.WriteLine("----Bienvenido al juego ahorcado----");
+Console.WriteLine("***Tendrá 5 intentos para intentar adivinar la palabra\n");
+Ahorcado a1 = new Ahorcado();
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine($"Intento {i}, ingrese una letra:");
+    char letraUsuario = char.Parse(Console.ReadLine());
+    a1.agregarLetra(letraUsuario);
+    contIntentos++;
+    if (a1.comprobarGanador() == true)
+    {
+        Console.WriteLine($"\nFelicitaciones, usted ha ganado. La palabra era {a1.Palabra} y la ha adivinado en {contIntentos} intentos!");
+        break;
+    }
+}
+if (a1.comprobarGanador() == false)
+{
+    Console.WriteLine($"\nUsted ha perdido, la palabra era {a1.Palabra}");
+}  
