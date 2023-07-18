28d27e3643b01e7c02a37a97120ce20f3be98662
Juan Diego Derewicki <88844221+JuandieDerewicki@users.noreply.github.com>
2023-07-18 12:39:41 -0300

    Add files via upload

---
+++ Ejercicio Calcular Moda/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioModa2
+{
+    internal class Moda
+    {
+        public string FuncionModa(int[] matrizDatos)
+        {
+            int maxNumero = matrizDatos[0];
+            int maxVeces = 0;
+
+            for (int i = 0; i < matrizDatos.Length; i++) // Recorro cada numero de la matriz
+            {
+                int numVeces = 0; // Inicializo en 0 para que se actualice el conteo de cada numero
+                for (int j = 0; j < matrizDatos.Length; j++) // Recorro para saber cuantas veces se repite el numero
+                {
+                    if (matrizDatos[i] == matrizDatos[j])
+                    {
+                        numVeces++;
+                    }
+                    if (numVeces > maxVeces)
+                    {
+                        maxNumero = matrizDatos[i];
+                        maxVeces = numVeces;
+                    }
+                }
+            }
+            return "La moda en el conjunto de datos es " + maxNumero + " y se repite " + maxVeces + " veces";
+        }
+
+        public string FuncionValorMinMax(int[] matrizDatos)
+        {
+            int numMaximo = matrizDatos[0];
+            int numMinimo = matrizDatos[0];
+            foreach (int i in matrizDatos)
+            {
+                if (i > numMaximo)
+                {
+                    numMaximo = i;
+                }
+                if (i < numMinimo)
+                {
+                    numMinimo = i;  
+                }
+            }
+            return "El valor máximo en el conjunto de datos es " + numMaximo + " y el valor mínimo es " + numMinimo;
+        }
+    }
+}
---
+++ Ejercicio Calcular Moda/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EjercicioModa2;
+
+Moda m1 = new Moda();
+
+Console.Write("Ingrese números enteros separados por espacios: ");
+string input = Console.ReadLine(); // Cadena de datos que pone el usuario
+string[] inputNum = input.Trim().Split(' '); // Los espacios en blanco que se pongan al principio o al final se eliminan para evitar errores. Ademas el split hace que por cada espacio en blanco se salte a otro linea
+
+// Convertir las cadenas de entrada a enteros
+int[] numeros = new int[inputNum.Length];
+for (int i = 0; i < inputNum.Length; i++)
+{
+    if (int.TryParse(inputNum[i], out int num)) // Trato de pasar lo que tengo en inputNum que es una cadena de strings a un entero con int y si lo logra, almacena ese valor en el nuevo arreglo, y si no lo logra arroja un error
+    {
+        numeros[i] = num;
+    }
+    else
+    {
+        Console.WriteLine($"Invalid input: {inputNum[i]}");
+        return;
+    }
+}
+
+Console.WriteLine("\n" + m1.FuncionValorMinMax(numeros));
+Console.WriteLine("\n" + m1.FuncionModa(numeros));
+
+/* Forma 1
+Console.WriteLine("Ingrese la cantidad de datos con las que cuenta: ");
+int cantidadDatos = int.Parse(Console.ReadLine());  
+
+int[] matrizDatosUsuario = new int[cantidadDatos];
+
+Console.WriteLine("\nAhora ingrese sus respectivos datos númericos:");
+
+for (int i = 0; i < matrizDatosUsuario.Length; i++)
+{
+    Console.WriteLine("-");
+    int datosUsuario = int.Parse(Console.ReadLine());
+    matrizDatosUsuario[i] = datosUsuario;
+}
+
+Console.WriteLine("\n" + m1.FuncionValorMinMax(matrizDatosUsuario));
+Console.WriteLine("\n" + m1.FuncionModa(matrizDatosUsuario));
+*/
+
+
+
+
